30941fe2e473f6286e5ed20d6c91137940a98e1b
Dka-11 <adikaahmad05@gmail.com>
2022-12-21 10:46:51 +0700

    Edit LoginTest - Adika

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -11,53 +11,123 @@ class LoginTest extends TestCase
      *
      * @return void
      */
-    public function test_case_001_Login()
+
+    //  Menguji Fungsionalitas fitur login
+
+    // TC_LOGIN_001 - Form Inputan Terisi Sempurna
+    // Start - TC_LOGIN_001
+
+    // public function test_Login_001()
+    // {
+    //     $user = new User([
+    //         'email' => 'user@example.com',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+
+    //     $user->save();
+
+    //     $response = $this->post('/login', [
+    //         'email' => 'adika123@gmail.com',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+
+    //     $response->assertResponseStatus(302);
+    // }
+
+    // End - TC_LOGIN_001
+
+    // --------------------
+
+    // TC_LOGIN_002 - Form inputan kosong
+    // Start - TC_LOGIN_002
+
+    // public function test_Login_002()
+    // {
+    //     $user = new User([
+    //         'email' => 'user@example.com',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+
+    //     $user->save();
+
+    //     $response = $this->post('/login', []);
+
+    //     $response->assertResponseStatus(302);
+    // }
+
+    // End - TC_LOGIN_002
+
+    // --------------------
+
+    // TC_LOGIN_003 - Form pada bagian email berisi email yang belum terdaftarkan
+    // Start - TC_LOGIN_003
+
+    // public function test_Login_003()
+    // {
+    //     $user = new User([
+    //         'email' => 'user@example.com',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+
+    //     $user->save();
+
+    //     $response = $this->post('/login', [
+    //         'email' => 'user"gmail.com',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+
+    //     $response->assertResponseStatus(302);
+    // }
+
+    // End - TC_LOGIN_003
+
+    // --------------------
+
+    // TC_LOGIN_004 - Form pada bagian email tidak terisi
+    // Start - TC_LOGIN_004
+
+    // public function test_Login_004()
+    // {
+    //     $user = new User([
+    //         'email' => 'user@example.com',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+
+    //     $user->save();
+
+    //     $response = $this->post('/login', [
+    //         'email' => '',
+    //         'password' => bcrypt('123456'),
+    //     ]);
+
+    //     $response->assertResponseStatus(302);
+    // }
+
+    // End - TC_LOGIN_004
+
+    // --------------------
+
+    // TC_LOGIN_005 - Form pada bagian password tidak terisi
+    // Start - TC_LOGIN_005
+
+    public function test_Login_005()
     {
-        $user = factory(User::class)->create([
+        $user = new User([
             'email' => 'user@example.com',
-            'password' => bcrypt($password = '123456')
+            'password' => bcrypt('123456'),
         ]);
+
         $user->save();
 
         $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => $password,
+            'email' => 'adika123@gmail.com',
+            'password' => bcrypt(''),
         ]);
 
         $response->assertResponseStatus(302);
-        $this->actingAs($user);
     }
 
-    public function test_case_002_Login()
-    {
-        $user = factory(User::class)->create([
-            'email' => 'user@example.com',
-            'password' => bcrypt($password = '123456')
-        ]);
+    // End - TC_LOGIN_005
 
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => $password,
-        ]);
-
-        $response->assertResponseStatus(302);
-        $this->actingAs($user);
-    }
-
-    // public function test_case_003_Login()
-    // {
-    //     $user = factory(User::class)->create([
-    //         'email' => 'user@example.com',
-    //         'password' => bcrypt($password = '123456')
-    //     ]);
-
-    //     $response = $this->post('/login', [
-    //         'email' => $user->email,
-    //         'password' => 'wwrong_password',
-    //     ]);
-
-    //     $response->assertRedirectedTo('/home');
-    //     $response->assertResponseStatus(302);
-    //     $this->actingAs($user);
-    // }
+    // --------------------
 }
